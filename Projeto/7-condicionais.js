ebf6c55b6494d237cdf1f8ca7f2ec4370047e5e1
Daniel Sena <daniels_araujo@outlook.com.br>
2025-05-27 19:19:41 -0300

    04.05 - Operadores Lógicos encadeados

--- Projeto/7-condicionais.js
+++ Projeto/7-condicionais.js
@@ -7,19 +7,25 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
-const estaAcompanhada = true;
+const estaAcompanhado = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
-    listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhada) {
-    console.log("Comprador está acompanhado");
+if (idadeComprador >= 18 || estaAcompanhado) {
+    console.log("Boa Viagem");
     listaDeDestinos.splice(1, 1);
 } else {
     console.log("Não é maior de idade e não posso vender");
+}
+
+console.log("Embarque:\n\n");
+
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa Viagem");
+} else {
+    console.log("Você não pode embarcar");
 }
 
 console.log(listaDeDestinos);
