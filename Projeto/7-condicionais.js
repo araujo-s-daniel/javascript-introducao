32288e5775a621128db208610e97445200640b78
Daniel Sena <daniels_araujo@outlook.com.br>
2025-05-27 19:08:17 -0300

    04.03 -  Ifs encadeados

--- Projeto/7-condicionais.js
+++ Projeto/7-condicionais.js
@@ -7,20 +7,19 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 15;
+const estaAcompanhada = true;
+
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
 if (idadeComprador >= 18) {
     console.log("Comprador maior de idade");
     listaDeDestinos.splice(1, 1);
+} else if (estaAcompanhada) {
+    console.log("Comprador está acompanhado");
+    listaDeDestinos.splice(1, 1);
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
 
 console.log(listaDeDestinos);
-
-console.log(idadeComprador > 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador == 18);
